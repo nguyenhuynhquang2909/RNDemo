72f1f735542d502c9b8c2647cab1cc92558dc7d8
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-04-12 13:55:36 +0700

    Update button

---
+++ RNDemo/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet} from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
+
+export default function App() {
+  return <StartGameScreen />
+}
+
+const styles = StyleSheet.create({
+  
+});
