fc44bf964d72dfa0eb58022fc8607d7e3e1abcca
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-04-12 14:54:11 +0700

    Finish styling button

--- RNDemo/components/PrimaryButton.js
+++ RNDemo/components/PrimaryButton.js
@@ -1,11 +1,42 @@
 
-import {View, Text} from 'react-native';
-
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+function pressHandler() {
+    console.log('Pressed');
+}
 function PrimaryButton({children}) {
     return (
-    <View>
-        <Text>{children}</Text>
-    </View>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable 
+            onPress={pressHandler} 
+            android_ripple={{color: "#640233"}} 
+            style={({pressed}) => pressed ? [styles.buttonInnrerContainer, styles.pressed]: styles.buttonInnrerContainer}
+             >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
     )   
 }
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnrerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        margin: 4,
+        elevation: 2
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+    pressed: {
+        opacity: 0.75,
+
+    }
+});
